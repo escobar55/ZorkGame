d3a81ec69586025b025e1f94ace94155a431282b
escobar55 <escobar5@umbc.edu>
2019-10-28 11:12:56 -0400

    roomtwo

--- src/ZorkAssignmentKsenia.java
+++ src/ZorkAssignmentKsenia.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 public class ZorkAssignmentKsenia {
 
@@ -60,3 +60,4 @@ public class ZorkAssignmentKsenia {
         }
 
 }
+*/
--- src/ZorkAssignmentSoheila.java
+++ src/ZorkAssignmentSoheila.java
@@ -18,10 +18,26 @@ public class ZorkAssignmentSoheila {
                 System.out.println("Goodbye!");
             } else if (answer == 1) {
                 answer = roomOne();
-            }
-        }
+                System.out.println(answer);
+            } else if (answer == 2) {
+                answer = roomTwo();
+            } /*else if (answer == 3) {
+                answer = roomThree();
+            } else if (answer == 4) {
+                answer = roomFour();
+            } else if (answer == 5) {
+                answer = roomFive();
+            } else if (answer == 6) {
+                answer = roomSix();
+            } else if (answer == 7) {
+                answer = roomSeven();
+            } else if (answer == 8) {
+                answer = roomEight();
+            } //end if statements
+            */
+        } //end while loop
 
-    }
+    } //end main method
 
     public static int roomOne() {
         Scanner keyboard = new Scanner(System.in);
@@ -40,8 +56,31 @@ public class ZorkAssignmentSoheila {
             } else {
                 System.out.println("Please enter a valid choice (N: North to room 2, Q: to exit castle): ");
             }
-        }
+        } //end while loop - room 1
 
-    }
+    } //end-room one method
 
-}
+    public static int roomTwo() {
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+
+        System.out.println("Welcome to room 2, the front room.");
+        System.out.println("You see a piano.");
+        System.out.println("You can go South (S), or West (W), or East (E)");
+
+        while (true) {
+            answer = keyboard.nextLine();
+            if (answer.equalsIgnoreCase("S")) {
+                return 1;  // will call method roomOne from inside main()
+            } else if(answer.equalsIgnoreCase("w")){
+                return 3; // will call method roomThree from inside main()
+            } else if(answer.equalsIgnoreCase("e")){
+                return 4; // will call method roomFour from inside main()
+            }  else {
+                System.out.println("Please enter a valid choice (S: South back to room 1, W: to room 3, E: to room 4): ");
+            }
+        } //end while loop - room 2
+
+    } //end-room one method
+
+} //end main class
