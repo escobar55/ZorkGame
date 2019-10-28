ccf6e30fbd283306f6a704d8380b0117be7388c3
rose-lake <klake12@montgomerycollege.edu>
2019-10-28 11:27:34 -0400

    .gitignore undo

--- src/ZorkAssignmentKsenia.java
+++ src/ZorkAssignmentKsenia.java
@@ -19,14 +19,14 @@ public class ZorkAssignmentKsenia {
                 System.out.println("Goodbye!");
             } else if (answer == 1) {
                 answer = roomOne();
+            } else if (answer == 2) {
+                answer = roomTwo();
+            } else if (answer == 3) {
+                answer = roomThree();
             }
 
-            // rooms to be written
-//                else if (answer == 2) {
-//                    answer = roomTwo();
-//                } else if (answer == 3) {
-//                    answer = roomThree();
-//                } else if (answer == 4) {
+//rooms to be written
+//                else if (answer == 4) {
 //                    answer = roomFour();
 //                } else if (answer == 5) {
 //                    answer = roomFive();
@@ -62,7 +62,31 @@ public class ZorkAssignmentKsenia {
             }
         }
 
-    }
+    } // end room one method
+
+
+    public static int roomTwo() {
+        Scanner keyboard = new Scanner(System.in);
+        String answer;
+
+        System.out.println("Welcome to room 2, the front room.");
+        System.out.println("You see a piano.");
+        System.out.println("You can go South (S), or West (W), or East (E)");
+
+        while (true) {
+            answer = keyboard.nextLine();
+            if (answer.equalsIgnoreCase("S")) {
+                return 1;  // will call method roomOne from inside main()
+            } else if(answer.equalsIgnoreCase("w")){
+                return 3; // will call method roomThree from inside main()
+            } else if(answer.equalsIgnoreCase("e")){
+                return 4; // will call method roomFour from inside main()
+            }  else {
+                System.out.println("Please enter a valid choice (S: South back to room 1, W: to room 3, E: to room 4): ");
+            }
+        } //end while loop - room 2
+
+    } //end-room two method
 
     public static int roomThree() {
         Scanner keyboard = new Scanner(System.in);
@@ -83,6 +107,6 @@ public class ZorkAssignmentKsenia {
             }
         }
 
-    }
+    } // end room three method
 
 }
